3208269d8cb1d2aaf5644f8616d64da174654604
Sears_Brett <searb357@hsd.k12.or.us>
2016-10-21 15:39:35 -0700

    adding mab libs

---
+++ Mad libs.py
@@ -0,0 +1,37 @@
+name = input('Enter your name: ') 
+print('Hello', name)
+print('Today we are going to use input and output to make a mad lib generator') #Introduction
+
+noun = input('Enter a noun: ')
+noun2 = input('Enter a 2nd noun: ')
+noun3 = input('Enter a 3rd noun: ')
+occupation = input('Enter a occupation: ')
+verb = input('Enter a verb: ')
+place = input('Enter a place: ')
+verbendinged = input('Enter a verb ending in ED: ')
+noun4 = input('Enter a 4th noun: ')
+verbendinging = input('Enter a verb ending in ING: ')
+puralnoun = input('Enter a pural noun: ')
+emotion = input('Enter a emotion: ') #Input section
+
+print('Now we wait for the magical program to calculate this data')
+import time
+time.sleep(5)
+
+print('It was during the battle of', noun, 'when I was running through a', noun2, 'when a', noun3, 'went off right next to my platoon. Our', occupation, 'yelled for us to', verb, 'to the nearest', place, 'we could find. When we got to the', place, 'we', verbendinged, 'to start a fire. As we were starting the fire the enemy saw the', noun4, 'from the fire and started', verbendinging, 'at us. we all quickly ducked behind the', puralnoun, 'at the', place, 'and returned fire. we quickly eliminated the enemy and were', emotion, 'that we had won the battle.')
+#End of Mad libs
+import time
+time.sleep(20)
+
+#Start of math equation
+print('Welcome back', name, 'we are going to do some math')
+
+
+
+story = """
+Paste
+your
+story
+here.
+"""
+print(story)
